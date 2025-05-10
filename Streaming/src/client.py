0578d2410d3e5e7342bd561175a0a42799bddf39
HyukSangCho <a01045542949@gmail.com>
2025-05-10 14:20:32 +0900

    add price function

---
+++ Streaming/src/client.py
@@ -0,0 +1,29 @@
+from core import *
+
+class Api:
+    @staticmethod
+    def get_tickers(is_deatils=False):
+        """
+        빗썸이 지원하는 KRW 마켓 코드 30개
+        :param payment_currency : KRW
+        :return:
+        """
+        resp = None
+        try:
+            resp = PublicApi.ticker(is_deatils)
+
+            krw_ticker = [item["market"] for item in resp if item["market"].startswith("KRW-")]
+            krw_ticker = krw_ticker[:30]
+            return krw_ticker
+        except Exception:
+            return resp
+    
+    @staticmethod
+    def get_price(markets):
+        resp = None
+        try:
+            resp = PublicApi.price(markets)
+
+            return resp
+        except Exception:
+            return resp
